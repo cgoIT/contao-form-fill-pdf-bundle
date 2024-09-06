0b5c3445b9af6fa1bb188a88238b9a9923527fdd
cgoIT <info@cgo-it.de>
2024-09-06 11:40:19 +0200

    chore: fix ecs bugs

--- src/Widget/GeneratePdf.php
+++ src/Widget/GeneratePdf.php
@@ -18,7 +18,7 @@ use Contao\Widget;
 
 class GeneratePdf extends Widget
 {
-    public const string TYPE = 'fp_generate_pdf';
+    final public const string TYPE = 'fp_generate_pdf';
 
     /**
      * Submit indicator.
